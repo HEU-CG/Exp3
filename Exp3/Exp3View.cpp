715420c5814790e3a01fe4f8c18ae3e8a92568da
xhd0728 <xinhaidong0625@foxmail.com>
2023-05-24 10:51:31 +0800

    sphere

--- Exp3/Exp3View.cpp
+++ Exp3/Exp3View.cpp
@@ -177,24 +177,30 @@ void CExp3View::HideSphere(CDC* pDC, float R)
 
 	int idelta = 10;
 
-	for (double i = 0; i < 200; i += idelta)
+	for (double i = 0; i < 180; i += idelta)
 	{
+		double alpha = i * PI / 180;
+		double delta_alpha = (i + 10) * PI / 180;
 		for (int j = 0; j < 360; j += 5)
 		{
 			a1 = j * PI / 180;
 			a2 = (j + 5) * PI / 180;
-			x[0] = R * cos(a1);
-			y[0] = i;
-			z[0] = R * sin(a1);
-			x[3] = R * cos(a2);
-			y[3] = i;
-			z[3] = R * sin(a2);
-			x[2] = R * cos(a2);
-			y[2] = i + idelta;
-			z[2] = R * sin(a2);
-			x[1] = R * cos(a1);
-			y[1] = i + idelta;
-			z[1] = R * sin(a1);
+
+			x[0] = R * sin(alpha) * sin(a1);
+			y[0] = R * cos(alpha);
+			z[0] = R * sin(alpha) * cos(a1);
+
+			x[1] = R * sin(delta_alpha) * sin(a1);
+			y[1] = R * cos(delta_alpha);
+			z[1] = R * sin(delta_alpha) * cos(a1);
+
+			x[2] = R * sin(delta_alpha) * sin(a2);
+			y[2] = R * cos(delta_alpha);
+			z[2] = R * sin(delta_alpha) * cos(a2);
+
+			x[3] = R * sin(alpha) * sin(a2);
+			y[3] = R * cos(alpha);
+			z[3] = R * sin(alpha) * cos(a2);
 
 			xn = (y[1] - y[0]) * (z[2] - z[0]) - (y[2] - y[0]) * (z[1] - z[0]);//计算网格四边形外法向量
 			yn = (x[2] - x[0]) * (z[1] - z[0]) - (x[1] - x[0]) * (z[2] - z[0]);
@@ -206,12 +212,17 @@ void CExp3View::HideSphere(CDC* pDC, float R)
 			xv = ViewPointx - x[0];//视向量
 			yv = ViewPointy - y[0];
 			zv = ViewPointz - z[0];
+
+			//xv = x[0];//视向量
+			//yv = y[0];
+			//zv = z[0];
+
 			mv = sqrt(xv * xv + yv * yv + zv * zv);
 			xv = xv / mv;//规格化视向量
 			yv = yv / mv;
 			zv = zv / mv;
 			costheta = xn * xv + yn * yv + zn * zv;
-			if (costheta >= 0)//根据后向判别法实现消隐,cosθ>=0则画出小四边形
+			if (costheta >= -0.1)//根据后向判别法实现消隐,cosθ>=0则画出小四边形
 			{
 				for (int k = 0; k < 4; k++)//进行观察变换
 				{
@@ -231,49 +242,6 @@ void CExp3View::HideSphere(CDC* pDC, float R)
 			}
 			Sleep(SLEEP_TIME);
 		}
-	}
-	for (int ii = 0; ii < 360; ii += 5)
-	{
-		a1 = ii * PI / 180;
-		a2 = (ii + 5) * PI / 180;
-		y[0] = y[1] = y[2] = 200;
-		x[0] = R * cos(a1);
-		z[0] = R * sin(a1);
-		x[1] = 0;
-		z[1] = 0;
-		x[2] = R * cos(a2);
-		z[2] = R * sin(a2);
-		xn = (y[1] - y[0]) * (z[2] - z[0]) - (y[2] - y[0]) * (z[1] - z[0]);//计算网格四边形外法向量
-		yn = (x[2] - x[0]) * (z[1] - z[0]) - (x[1] - x[0]) * (z[2] - z[0]);
-		zn = (x[1] - x[0]) * (y[2] - y[0]) - (x[2] - x[0]) * (y[1] - y[0]);
-		vn = sqrt(xn * xn + yn * yn + zn * zn);//外法向量的模
-		xn = xn / vn;//规格化外法向量
-		yn = yn / vn;
-		zn = zn / vn;
-		xv = ViewPointx - x[0];//视向量
-		yv = ViewPointy - y[0];
-		zv = ViewPointz - z[0];
-		mv = sqrt(xv * xv + yv * yv + zv * zv);
-		xv = xv / mv;//规格化视向量
-		yv = yv / mv;
-		zv = zv / mv;
-		costheta = xn * xv + yn * yv + zn * zv;
-		if (costheta >= 0)//根据后向判别法实现消隐,cosθ>=0则画出小四边形
-		{
-			for (int k = 0; k < 4; k++)//进行观察变换
-			{
-				x1[k] = x[k] * Tk[3] - z[k] * Tk[1];//观察坐标系三维坐标
-				y1[k] = -x[k] * Tk[8] + y[k] * Tk[2] - z[k] * Tk[7];
-				z1[k] = -x[k] * Tk[6] - y[k] * Tk[4] - z[k] * Tk[5] + TR;
-				sx[k] = x1[k];
-				sy[k] = y1[k];
-			}
-			pDC->MoveTo((int)sx[0], (int)sy[0]);
-			pDC->LineTo((int)sx[1], (int)sy[1]);
-			pDC->LineTo((int)sx[2], (int)sy[2]);
-			pDC->LineTo((int)sx[0], (int)sy[0]);
-		}
-		Sleep(SLEEP_TIME);
 	}
 }
 
@@ -296,24 +264,30 @@ void CExp3View::FillSphere(CDC* pDC, float R)
 	Ie = Ia * ka;//环境光分量
 
 	int idelta = 10;
-	for (double i = 0; i < 200; i += idelta)
+	for (double i = 0; i < 120; i += idelta)
 	{
+		double alpha = i * PI / 180;
+		double delta_alpha = (i + 10) * PI / 180;
 		for (int j = 0; j < 360; j += 5)
 		{
 			a1 = j * PI / 180;
 			a2 = (j + 5) * PI / 180;
-			x[0] = R * cos(a1);
-			y[0] = i;
-			z[0] = R * sin(a1);
-			x[3] = R * cos(a2);
-			y[3] = i;
-			z[3] = R * sin(a2);
-			x[2] = R * cos(a2);
-			y[2] = i + idelta;
-			z[2] = R * sin(a2);
-			x[1] = R * cos(a1);
-			y[1] = i + idelta;
-			z[1] = R * sin(a1);
+
+			x[0] = R * sin(alpha) * sin(a1);
+			y[0] = R * cos(alpha);
+			z[0] = R * sin(alpha) * cos(a1);
+
+			x[1] = R * sin(delta_alpha) * sin(a1);
+			y[1] = R * cos(delta_alpha);
+			z[1] = R * sin(delta_alpha) * cos(a1);
+
+			x[2] = R * sin(delta_alpha) * sin(a2);
+			y[2] = R * cos(delta_alpha);
+			z[2] = R * sin(delta_alpha) * cos(a2);
+
+			x[3] = R * sin(alpha) * sin(a2);
+			y[3] = R * cos(alpha);
+			z[3] = R * sin(alpha) * cos(a2);
 
 			xn = (y[1] - y[0]) * (z[2] - z[0]) - (y[2] - y[0]) * (z[1] - z[0]);//计算网格四边形外法向量
 			yn = (x[2] - x[0]) * (z[1] - z[0]) - (x[1] - x[0]) * (z[2] - z[0]);
@@ -322,15 +296,20 @@ void CExp3View::FillSphere(CDC* pDC, float R)
 			xn = xn / vn;//规格化外法向量
 			yn = yn / vn;
 			zn = zn / vn;
+			//xv = x[0];//视向量
+			//yv = y[0];
+			//zv = z[0];
+
 			xv = ViewPointx - x[0];//视向量
 			yv = ViewPointy - y[0];
 			zv = ViewPointz - z[0];
+
 			mv = sqrt(xv * xv + yv * yv + zv * zv);
 			xv = xv / mv;//规格化视向量
 			yv = yv / mv;
 			zv = zv / mv;
 			costheta = xn * xv + yn * yv + zn * zv;
-			if (costheta >= 0)//根据后向判别法实现消隐,cosθ>=0则画出小四边形
+			if (costheta >= -0.25)//根据后向判别法实现消隐,cosθ>=0则画出小四边形
 			{
 				lx = LPx - x[0]; ly = LPy - y[0]; lz = LPz - z[0];//光源向量
 				mv = sqrt(lx * lx + ly * ly + lz * lz);
@@ -374,75 +353,6 @@ void CExp3View::FillSphere(CDC* pDC, float R)
 			Sleep(SLEEP_TIME);
 		}
 	}
-	for (int ii = 0; ii < 360; ii += 5)
-	{
-		a1 = ii * PI / 180;
-		a2 = (ii + 5) * PI / 180;
-		y[0] = y[1] = y[2] = 200;
-		x[0] = R * cos(a1);
-		z[0] = R * sin(a1);
-		x[1] = 0;
-		z[1] = 0;
-		x[2] = R * cos(a2);
-		z[2] = R * sin(a2);
-		xn = (y[1] - y[0]) * (z[2] - z[0]) - (y[2] - y[0]) * (z[1] - z[0]);//计算网格四边形外法向量
-		yn = (x[2] - x[0]) * (z[1] - z[0]) - (x[1] - x[0]) * (z[2] - z[0]);
-		zn = (x[1] - x[0]) * (y[2] - y[0]) - (x[2] - x[0]) * (y[1] - y[0]);
-		vn = sqrt(xn * xn + yn * yn + zn * zn);//外法向量的模
-		xn = xn / vn;//规格化外法向量
-		yn = yn / vn;
-		zn = zn / vn;
-		xv = ViewPointx - x[0];//视向量
-		yv = ViewPointy - y[0];
-		zv = ViewPointz - z[0];
-		mv = sqrt(xv * xv + yv * yv + zv * zv);
-		xv = xv / mv;//规格化视向量
-		yv = yv / mv;
-		zv = zv / mv;
-		costheta = xn * xv + yn * yv + zn * zv;
-		if (costheta >= 0)//根据后向判别法实现消隐,cosθ>=0则画出小四边形
-		{
-			lx = LPx - x[0]; ly = LPy - y[0]; lz = LPz - z[0];//光源向量
-			mv = sqrt(lx * lx + ly * ly + lz * lz);
-			lx = lx / mv; ly = ly / mv; lz = lz / mv;//规格化光源向量
-
-			costhita1 = xn * lx + yn * ly + zn * lz;//法向量与光源向量的夹角余弦
-			hx = lx + xv; hy = ly + yv; hz = lz + zv;//视向量与光源向量的和
-			mv = sqrt(hx * hx + hy * hy + hz * hz);
-			hx = hx / mv; hy = hy / mv; hz = hz / mv;
-			cosphi = xn * hx + yn * hy + zn * hz;//镜面反射光与视向量的夹角
-			if (costhita1 >= 0)
-				Id = Ip * kd * costhita1;
-			else
-				Id = 0;
-			if (cosphi >= 0)
-				Is = Ip * ks * pow(cosphi, ns);
-			else
-				Is = 0;
-
-			C = (int)(Ie + Id + Is);
-			if (C > 255) C = 255;
-
-			for (int k = 0; k < 3; k++)//进行观察变换
-			{
-				x1[k] = x[k] * Tk[3] - z[k] * Tk[1];//观察坐标系三维坐标
-				y1[k] = -x[k] * Tk[8] + y[k] * Tk[2] - z[k] * Tk[7];
-				z1[k] = -x[k] * Tk[6] - y[k] * Tk[4] - z[k] * Tk[5] + TR;
-
-				rgnpoints[k].x = x1[k];
-				rgnpoints[k].y = y1[k];
-			}
-			fillColor = RGB(C, C, C);
-			if (!brush.CreateSolidBrush(fillColor)) return;
-			if (!NewPen.CreatePen(PS_SOLID, 1, fillColor)) return;
-			pDC->SelectObject(&NewPen);
-			pDC->SelectObject(&brush);
-			pDC->Polygon(rgnpoints, 3);
-			brush.DeleteObject();
-			NewPen.DeleteObject();
-		}
-		Sleep(SLEEP_TIME);
-	}
 }
 
 // CExp3View 诊断
@@ -479,7 +389,7 @@ void CExp3View::OnAFill()
 	pDC->SetWindowExt(rect.Width(), rect.Height());
 	pDC->SetViewportExt(rect.Width(), -rect.Height());
 	pDC->SetViewportOrg(rect.Width() / 2, rect.Height() / 2);
-	FillSphere(pDC, 100);
+	FillSphere(pDC, 200);
 	ReleaseDC(pDC);
 }
 
@@ -494,6 +404,6 @@ void CExp3View::OnADraw()
 	pDC->SetWindowExt(rect.Width(), rect.Height());
 	pDC->SetViewportExt(rect.Width(), -rect.Height());
 	pDC->SetViewportOrg(rect.Width() / 2, rect.Height() / 2);
-	HideSphere(pDC, 100);
+	HideSphere(pDC, 200);
 	ReleaseDC(pDC);
 }
